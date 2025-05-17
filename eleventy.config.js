de4221436e11a7434ff556948f083463281d6c51
killeik <69602482+killeik@users.noreply.github.com>
2025-05-17 15:56:09 +0300

    less manual feed looks better now

--- eleventy.config.js
+++ eleventy.config.js
@@ -31,24 +31,24 @@ export default async function (eleventyConfig) {
     return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
   });
 
- //  eleventyConfig.addPlugin(feedPlugin, {
-	// 	type: "atom", // or "rss", "json"
-	// 	outputPath: "/feed.xml",
-	// 	collection: {
-	// 		name: "all", // iterate over `collections.all` - all the pages
-	// 		limit: 0,     // 0 means no limit
-	// 	},
-	// 	metadata: {
-	// 		language: "en",
-	// 		title: "killeik grimoire",
-	// 		subtitle: "personal magical book on the web",
-	// 		base: "https://killeik.net/",
-	// 		author: {
-	// 			name: "killeik",
-	// 			email: "qadol4zpb@mozmail.com", // Optional
-	// 		}
-	// 	}
-	// });
+  eleventyConfig.addPlugin(feedPlugin, {
+		type: "atom", // or "rss", "json"
+		outputPath: "/feed.xml",
+		collection: {
+			name: "all", // iterate over `collections.all` - all the pages
+			limit: 0,     // 0 means no limit
+		},
+		metadata: {
+			language: "en",
+			title: "killeik grimoire",
+			subtitle: "personal magical book on the web",
+			base: "https://killeik.net/",
+			author: {
+				name: "killeik",
+				email: "qadol4zpb@mozmail.com", // Optional
+			}
+		}
+	});
 
   let markdownOptions = {
     html: true,
